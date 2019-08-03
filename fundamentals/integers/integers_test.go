6b698c248454536423615991560234a585271500
Valdemar José Ceccon Junior <valdemar.ceccon@gmail.com>
2019-08-03 00:25:27 -0300

    integers test (#1)
    example

---
+++ fundamentals/integers/integers_test.go
@@ -0,0 +1,39 @@
+package integers_test
+
+import (
+	"fmt"
+	"github.com/valdemarceccon/golang-tdd-study/fundamentals/integers"
+	"testing"
+)
+
+func TestAdder(t *testing.T) {
+
+	assertCorrectSum := func(t *testing.T, expected, got int) {
+		t.Helper()
+
+		if got != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, got)
+		}
+	}
+
+	t.Run("when adding 2 and 2 then result is 4", func(t *testing.T) {
+		sum := integers.Add(2, 2)
+		expected := 4
+
+		assertCorrectSum(t, expected, sum)
+	})
+
+	t.Run("when adding 1 and 5 then result is 6", func(t *testing.T) {
+		sum := integers.Add(1, 5)
+		expected := 6
+
+		assertCorrectSum(t, expected, sum)
+	})
+
+}
+
+func ExampleAdd() {
+	sum := integers.Add(1, 5)
+	fmt.Println(sum)
+	//Output: 6
+}
