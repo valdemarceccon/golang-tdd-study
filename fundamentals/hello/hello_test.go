d7f02c37d0f5561e3fb6e1df4e350a0c904e9c60
Valdemar Ceccon <valdemar.ceccon@gmail.com>
2019-08-03 00:03:50 -0300

    tests refactored second passing test done Hello now has especial treatment for empty string

--- fundamentals/hello/hello_test.go
+++ fundamentals/hello/hello_test.go
@@ -6,10 +6,25 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := hello.Hello("Valdemar")
-	want := "Hello, Valdemar"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := hello.Hello("Valdemar")
+		want := "Hello, Valdemar"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, World' when a empty string is supplied", func(t *testing.T) {
+		got := hello.Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
