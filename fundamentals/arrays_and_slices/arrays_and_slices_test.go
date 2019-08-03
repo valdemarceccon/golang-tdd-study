c7ec74f2eabbfd5ef836fa31082a9c31bfc5429c
Valdemar José Ceccon Junior <valdemar.ceccon@gmail.com>
2019-08-03 01:44:59 -0300

    Arrays and slices (#3)
    * arrays and slices tests
    
    * arrays and slices refactoring and another test scenarios

---
+++ fundamentals/arrays_and_slices/arrays_and_slices_test.go
@@ -0,0 +1,57 @@
+package arrays_and_slices_test
+
+import (
+	"github.com/valdemarceccon/golang-tdd-study/fundamentals/arrays_and_slices"
+	"reflect"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := arrays_and_slices.Sum(numbers)
+		want := 15
+
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{4, 5, 6, 7}
+
+		got := arrays_and_slices.Sum(numbers)
+		want := 22
+
+		if want != got {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of slices", func(t *testing.T) {
+		got := arrays_and_slices.SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := arrays_and_slices.SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+	})
+
+}
---
+++ fundamentals/arrays_and_slices/sum.go
@@ -0,0 +1,23 @@
+package arrays_and_slices
+
+func Sum(numbers []int) (sum int) {
+	sum = 0
+
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
