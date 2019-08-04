a6aa3eb16bd14d1b4757690d052ede70ea986347
Valdemar Ceccon <valdemar.ceccon@gmail.com>
2019-08-04 02:27:55 -0300

    Squashed commit of the following:
    commit 4f36d794ddd7d912900bf71fcc01c78734d32179
    Author: Valdemar Ceccon <valdemar.ceccon@gmail.com>
    Date:   Sun Aug 4 02:26:53 2019 -0300
    
        implemented walk function to accept any type e return all string that it contains

---
+++ fundamentals/12_reflection/reflection.go
@@ -0,0 +1,36 @@
+package reflection
+
+import "reflect"
+
+func Walk(x interface{}, fn func(string)) {
+	val := getValue(x)
+
+	walkValue := func(value reflect.Value) {
+		Walk(value.Interface(), fn)
+	}
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	return val
+}
