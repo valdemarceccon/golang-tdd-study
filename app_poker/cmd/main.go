36a4b40948ee01e2a137bfedf333386145166f6c
Valdemar Ceccon <valdemar.ceccon@gmail.com>
2019-08-08 01:37:24 -0300

    wrapping up user interfaces

--- app_poker/cmd/main.go
+++ app_poker/cmd/main.go
@@ -2,8 +2,24 @@ package cmd
 
 import (
 	"fmt"
+	poker "github.com/valdemarceccon/golang-tdd-study/app_poker"
+	"github.com/valdemarceccon/golang-tdd-study/app_poker/cmd/cli"
+	"log"
+	"os"
 )
 
+const dbFileName = "game.db.json"
+
 func main() {
+	store, closeDB, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer closeDB()
+
 	fmt.Println("Let's play poker")
+	fmt.Println("Type {Name} wins to record a win")
+	cli.NewCLI(store, os.Stdin).PlayPoker()
 }
