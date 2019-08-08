17f506fc496d12af529d8fcf22234dc9da5ba46f
Valdemar Ceccon <valdemar.ceccon@objective.com.br>
2019-08-08 13:38:06 -0300

    playpoker now have fixed values and times for blind

--- app_poker/cmd/cli/cli_test.go
+++ app_poker/cmd/cli/cli_test.go
@@ -1,6 +1,7 @@
 package cli_test
 
 import (
+	"fmt"
 	cli2 "github.com/valdemarceccon/golang-tdd-study/app_poker/cmd/cli"
 	"github.com/valdemarceccon/golang-tdd-study/app_poker/player"
 	"github.com/valdemarceccon/golang-tdd-study/app_poker/pokertesting"
@@ -46,16 +47,51 @@ func TestCLI(t *testing.T) {
 		pokertesting.AssertPlayerWin(t, playerStore, "Cleo")
 	})
 
-	t.Run("it schedules priting of blind values", func(t *testing.T) {
+	t.Run("it schedules printing of blind values", func(t *testing.T) {
 		in := strings.NewReader("Chris wins\n")
-		stubPlayerStore := &player.StubPlayerStore{}
+		playerStore := &player.StubPlayerStore{}
 		blindAlerter := &SpyBlindAlerter{}
 
-		cli := cli2.NewCLI(stubPlayerStore, in, blindAlerter)
+		cli := cli2.NewCLI(playerStore, in, blindAlerter)
 		cli.PlayPoker()
 
-		if len(blindAlerter.alerts) != 1 {
-			t.Fatal("expected a blind alert to be scheduled")
+		cases := []struct {
+			expectedScheduleTime time.Duration
+			expectedAmount       int
+		}{
+			{0 * time.Second, 100},
+			{10 * time.Minute, 200},
+			{20 * time.Minute, 300},
+			{30 * time.Minute, 400},
+			{40 * time.Minute, 500},
+			{50 * time.Minute, 600},
+			{60 * time.Minute, 800},
+			{70 * time.Minute, 1000},
+			{80 * time.Minute, 2000},
+			{90 * time.Minute, 4000},
+			{100 * time.Minute, 8000},
+		}
+
+		for i, c := range cases {
+			t.Run(fmt.Sprintf("%d scheduled for %v", c.expectedAmount, c.expectedScheduleTime), func(t *testing.T) {
+				if len(blindAlerter.alerts) <= i {
+					t.Fatalf("alert %d wat not scheduled %v", i, blindAlerter.alerts)
+				}
+
+				alert := blindAlerter.alerts[i]
+
+				amountGot := alert.amount
+
+				if amountGot != c.expectedAmount {
+					t.Fatalf("got amount %d, want %v", amountGot, c.expectedAmount)
+				}
+
+				gotScheduleTime := alert.scheduledAt
+
+				if gotScheduleTime != c.expectedScheduleTime {
+					t.Errorf("got schedule time of %v, want %v", gotScheduleTime, c.expectedScheduleTime)
+				}
+			})
 		}
 	})
 }
